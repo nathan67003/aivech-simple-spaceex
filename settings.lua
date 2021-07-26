ae2312435853a7204e5661623cc2356127d4c7ff
Aivech <16143392+Aivech@users.noreply.github.com>
2021-07-26 03:42:58 -0400

    Added mod settings. Does not do anything yet.

---
+++ settings.lua
@@ -0,0 +1,37 @@
+data:extend({
+---- AAI burner control
+-- Gate electrical power behind AAI?
+  {
+    type = "bool-setting",
+   name = "aivech-ssx-gate-electricity",
+   setting_type = "startup",
+   default_value = false,
+   order = "a-aai"
+  },
+  -- Use AAI intermediates
+  {
+   type = "bool-setting",
+   name = "aivech-ssx-aai-recipes",
+   setting_type = "startup",
+   default_value = false,
+   order = "a-aai"
+  },
+  -- Offshore pump requires electricity
+  {
+   type = "bool-setting",
+   name = "aivech-ssx-offshore-pump",
+   setting_type = "startup",
+   default_value = false,
+   order = "a-aai"
+  },
+---- Space Exploration settings
+  -- Low density structure options
+  {
+   type = "string-setting",
+   name = "aivech-ssx-low-density-structure",
+   setting_type = "startup",
+   default_value = "replace",
+   allowed_values = {"ignore","replace","revert"},
+   order = "b-space-ex"
+  }
+})
